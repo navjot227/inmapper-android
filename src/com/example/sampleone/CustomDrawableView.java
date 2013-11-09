1bcdcd93635ed8b29d9da181a56624d21d70480c
Navjot Singh <anavjot@yahoo.com>
2013-11-09 17:10:33 -0500

    Committed local changes for integration

--- src/com/example/sampleone/CustomDrawableView.java
+++ src/com/example/sampleone/CustomDrawableView.java
@@ -9,37 +9,37 @@ import android.graphics.drawable.shapes.OvalShape;
 import android.view.View;
 
 public class CustomDrawableView extends View {
-	private ShapeDrawable mDrawable;
-	private Random rand = new Random();
+    private final ShapeDrawable mDrawable;
+    private final Random rand = new Random();
 
-	public CustomDrawableView(Context context) {
-		super(context);
+    public CustomDrawableView(Context context) {
+        super(context);
 
-		int x = 10;
-		int y = 10;
-		int width = 300;
-		int height = 50;
+        int x = 10;
+        int y = 10;
+        int width = 300;
+        int height = 50;
 
-		mDrawable = new ShapeDrawable(new OvalShape());
-		mDrawable.getPaint().setColor(0xff74AC23);
-		mDrawable.setBounds(x, y, x + width, y + height);
-	}
+        mDrawable = new ShapeDrawable(new OvalShape());
+        mDrawable.getPaint().setColor(0xff74AC23);
+        mDrawable.setBounds(x, y, x + width, y + height);
+    }
 
-	protected void onDraw(Canvas canvas) {
-		mDrawable.draw(canvas);
-	}
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mDrawable.draw(canvas);
+    }
 
-	public void changeShape() {
-//		while (true) {
-	/*		try {
-				Thread.currentThread();
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
-			mDrawable.setBounds(10,10,300+rand.nextInt(500),50+rand.nextInt(200));
-			
-		}
-	//}
+    public void changeShape() {
+        // while (true) {
+        /*
+         * try { Thread.currentThread(); Thread.sleep(10000); } catch
+         * (InterruptedException e) { // TODO Auto-generated catch block
+         * e.printStackTrace(); }
+         */
+        mDrawable.setBounds(10, 10, 300 + rand.nextInt(500),
+                50 + rand.nextInt(200));
+
+    }
+
 }
--- src/com/example/sampleone/MapActivity.java
+++ src/com/example/sampleone/MapActivity.java
@@ -1,39 +1,66 @@
 package com.example.sampleone;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 
+import com.example.sampleone.MappingService.RoomPointList;
+
 public class MapActivity extends Activity {
-	private CustomDrawableView mCustomDrawableView;
-	private final Handler handler = new Handler();
+    private CustomDrawableView mCustomDrawableView;
+    private final Handler handler = new Handler();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
-		mCustomDrawableView = new CustomDrawableView(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_map);
+        mCustomDrawableView = new CustomDrawableView(this);
+        // mCustomDrawableView.changeShape();
+        setContentView(mCustomDrawableView);
+        new RetreiveMapPoints().execute();
+    }
 
-		setContentView(mCustomDrawableView);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.map, menu);
+        // doTheAutoRefresh();
+        return true;
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.map, menu);
-		doTheAutoRefresh();
-		return true;
-	}
+    public void doTheAutoRefresh() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mCustomDrawableView.invalidate();
+                doTheAutoRefresh();
+            }
+        }, 1000);
+    }
 
-	public void doTheAutoRefresh() {
-		handler.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				mCustomDrawableView.changeShape();
-				mCustomDrawableView.invalidate();
-				doTheAutoRefresh();
-			}
-		}, 1000);
-	}
+    class RetreiveMapPoints extends AsyncTask<String, Void, RoomPointList> {
+        @Override
+        protected RoomPointList doInBackground(String... urls) {
+            return new MappingService().requestMapping();
+
+        }
+
+        @Override
+        protected void onPostExecute(RoomPointList result) {
+            // System.out.println("result.length = " + result.size());
+            for (RoomPointTo roomPointTo : result.getUserMappings().values()
+                    .iterator().next()) {
+                System.out.println(roomPointTo.getMobileId());
+                System.out.println(roomPointTo.getRoomId());
+                System.out.println(roomPointTo.getX());
+                System.out.println(roomPointTo.getY());
+
+            }
+            // TODO: check this.exception
+            // TODO: do something with the feed
+        }
+    }
+
 }
